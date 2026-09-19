d9457e683aac5d6fa9de2e6c2cee13fdd96f7eff
DANYA <danyavolk000@gmail.com>
2026-09-19 13:23:38 +0300

    Create topics API!

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Topic
+from .serializers import TopicSerializer
 
-# Create your views here.
+
+class TopicListCreateView(generics.ListCreateAPIView):
+    queryset = Topic.objects.all().order_by("-created_at")
+    serializer_class = TopicSerializer
+
+
+class TopicDetailView(generics.RetrieveAPIView):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
