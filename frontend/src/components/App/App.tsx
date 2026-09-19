46945f3053742c87fbabed1a2d557310dbde4edb
Johnny <itdevjs@icloud.com>
2026-09-20 02:54:49 +0300

    add tanstak & zustand & axios

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from '../Layout/Layout';
+
+export const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
